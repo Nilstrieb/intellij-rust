86ac440f47e5abece1438302dbdc76c157355639
mchernyavsky <chernyavsky.mikhail@gmail.com>
2022-05-29 21:45:23 +0300

    Check EDT and ReadAction before accessing to WSL file system

--- src/main/kotlin/org/rust/cargo/toolchain/wsl/Utils.kt
+++ src/main/kotlin/org/rust/cargo/toolchain/wsl/Utils.kt
@@ -6,6 +6,9 @@
 package org.rust.cargo.toolchain.wsl
 
 import com.intellij.execution.wsl.WSLDistribution
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
+import org.jetbrains.annotations.NonNls
 import java.nio.file.Path
 
 fun WSLDistribution.expandUserHome(path: String): String {
@@ -14,6 +17,22 @@ fun WSLDistribution.expandUserHome(path: String): String {
     return "$userHome${path.substring(1)}"
 }
 
-fun Path.hasExecutableOnWsl(toolName: String): Boolean = pathToExecutableOnWsl(toolName).toFile().isFile
+fun Path.hasExecutableOnWsl(toolName: String): Boolean {
+    checkEdtAndReadAction()
+    return pathToExecutableOnWsl(toolName).toFile().isFile
+}
 
 fun Path.pathToExecutableOnWsl(toolName: String): Path = resolve(toolName)
+
+private val LOG: Logger = Logger.getInstance("org.rust.cargo.toolchain.Utils")
+
+private fun checkEdtAndReadAction() {
+    val application = ApplicationManager.getApplication() ?: return
+    if (!application.isInternal || application.isHeadlessEnvironment) return
+    @NonNls val message = when {
+        application.isDispatchThread -> "Access to WSL filesystem on EDT"
+        application.isReadAccessAllowed -> "Access to WSL filesystem under ReadAction"
+        else -> return
+    }
+    LOG.error(message)
+}
